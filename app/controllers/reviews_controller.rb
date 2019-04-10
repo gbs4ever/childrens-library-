0ed15e70896b6d581579c4a1f9eb11b259b3130c
gbs4ever <gbs4ever2@gmail.com>
2019-04-10 00:51:28 -0400

    review views done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+  def index
+  end
+  def new 
+ @review = Review.new 
+    
+  end
+  def create
+     binding.pry
+    var =current_user.reviews.create(review_params)
+   
+  end
+
+
+
+private
+def review_params
+params.require(:review).permit(:comments,:status,:user_id,:book_id)
+end
+
+
 end
