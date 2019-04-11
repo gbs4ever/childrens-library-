b451960869b0bb22e24b5979faf0b65f9dfa4b54
gbs4ever <gbs4ever2@gmail.com>
2019-04-11 00:47:14 -0400

    check out books working still missing date

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,2 +1,32 @@
 class CheckoutsController < ApplicationController
+
+  def new
+   @checkout =   current_user.checkouts.build
+  end
+  def create
+   var = current_user.checkouts.build(checkout_params)
+   if  var.save  
+     redirect_to checkout_path(var)
+    else
+     render :new 
+    end
+  end
+  def show
+   @checkout = Checkout.find(params[:id])
+  end
+
+
+
+ def checkout_params
+    params.require(:checkout).permit(:user_id,:book_id)
+  end
+
+
+
+
+
 end
+
+
+
+
