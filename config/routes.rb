5a68f46a1412f700c604e747deb78c025a15252f
gbs4ever <gbs4ever2@gmail.com>
2019-04-11 23:42:33 -0400

    delted admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+
   
-  resources :reviews, only: [:new, :create]
-  resources :checkouts, only: [:new, :create, :index]
-  resources :books , only: [:new, :create, :index,:show,:edit,:update]
+  resources :reviews, only: [:new, :create,:index]
+  resources :checkouts, only: [:new, :create, :index, :show]
+  resources :books  do
+     resources :reviews, only: [:new, :create,:show]
+  end
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root  to: 'welcome#home'
