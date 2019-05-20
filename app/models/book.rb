182a08fe7eafc6e8465dd40e519c3570e6b369c0
gbs4ever <gbs4ever2@gmail.com>
2019-05-20 12:05:35 -0400

    fixed associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,11 @@
 class Book < ApplicationRecord
     validates :title, :presence => true
     validates :author, :presence => true
+    has_many :publishers
     has_many :checkouts, dependent: :destroy
-    has_many :users, -> {distinct}, through: :checkouts
+   
+    has_many :users, -> {distinct}, through: :publishers
+    #has_many :users, -> {distinct}, through: :checkouts 
     has_many :reviews, dependent: :destroy
     scope :title, -> { order(title: :asc)}
     scope :author, -> { order(author: :asc)}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-   has_many :checkouts
-    has_many :books, through: :checkouts
+    has_many :publishers
+    has_many :books, through: :publishers
+    has_many :checkouts
+    #has_many :books, through: :checkouts
     has_many :reviews
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
